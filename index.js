0cd19f12f5174db616568e57880a0aadb9b35b5e
lineMarques <aline.marques18@gmail.com>
2023-10-24 20:07:47 -0300

    força de senha terminado

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ function verificaForcaSenha() {
   mostrarSenha(inputSenha);
 
   inputSenha.addEventListener("keyup", function (event) {
-    senha = inputSenha.value;
-
-    let snNumero = /[0-9]/;
-    let snLetraMaiscula = /[A-Z]/;
-    let snLetraMinuscula = /[a-z]/;
-    let snCaractereEspecial = /[@!#$%^&*()/\\ ]/;
-
+    let senha = inputSenha.value;
+    let snNumero = 0;
+    let snMaiscula = 0;
+    let snMinuscula = 0;
+    let snCaracteresEspc = 0;
 
     if (senha.length > 1) {
       forcaSenha = 1;
@@ -28,35 +26,65 @@ function verificaForcaSenha() {
       forcaSenha = 5;
     }
 
+    for (let i = 0; i < senha.length; i++) {
+      /* Verificando se senha tem numeros */
+      if (senha.charCodeAt([i]) >= 48 && senha.charCodeAt([i]) <= 57) {
+        snNumero = 1;
+      }
 
-    if (senha.match(snNumero)) {
-      console.log("entrou");
-      forcaSenha += 1;
+      /* Verificando se senha tem letras maiusculas */
+      if (senha.charCodeAt([i]) >= 65 && senha.charCodeAt([i]) <= 90) {
+        snMaiscula = 1;
+      }
+
+      /* Verificando se senha tem letras minusculas */
+      if (senha.charCodeAt([i]) >= 97 && senha.charCodeAt([i]) <= 122) {
+        snMinuscula = 1;
+      }
+
+      /* Verificando se senha tem caracteres especiais */
+      if (
+        senha.charCodeAt([i]) != (senha.charCodeAt([i]) >= 48 && senha.charCodeAt([i]) <= 57) &&
+        senha.charCodeAt([i]) != (senha.charCodeAt([i]) >= 65 && senha.charCodeAt([i]) <= 90) &&
+        senha.charCodeAt([i]) != (senha.charCodeAt([i]) >= 97 && senha.charCodeAt([i]) <= 122)
+      ) {
+        snCaracteresEspc = 2;
+      }
     }
-    if (snLetraMinuscula.test(senha)) {
-      forcaSenha += 1;
+
+    /* Soma dos pontos para definir a força da senha */
+    forcaSenha += snNumero + snMaiscula + snMinuscula + snCaracteresEspc;
+
+    const divCorForca = document.getElementById("corForca");
+
+    if (forcaSenha <= 5) {  
+      divCorForca.classList.add("fraca");
+      divCorForca.classList.remove("media");
+      divCorForca.classList.remove("forte");
+    } else if (forcaSenha > 5 && forcaSenha < 9) {
+      divCorForca.classList.remove("fraca");
+      divCorForca.classList.add("media");
+      divCorForca.classList.remove("forte");
+    } else {
+      divCorForca.classList.remove("fraca");
+      divCorForca.classList.remove("media");
+      divCorForca.classList.add("forte");
     }
-    if (snLetraMaiscula.test(senha)) {
-      forcaSenha += 1;
-    }
-    if (snCaractereEspecial.test(senha)) {
-      forcaSenha += 2;
-    } 
- 
-    console.log("f", forcaSenha);
+
+    console.log(forcaSenha);
   });
 }
 
 function mostrarSenha(inputSenha) {
-  const inHide = document.getElementById("idShowSenha");
+  const inputHide = document.getElementById("idShowSenha");
 
-  inHide.addEventListener("click", function (event) {
+  inputHide.addEventListener("click", function (event) {
     if (inputSenha.type == "password") {
       inputSenha.type = "text";
-      inHide.value = "HIDE";
+      inputHide.value = "Esconder";
     } else {
       inputSenha.type = "password";
-      inHide.value = "SHOW";
+      inputHide.value = "Mostrar";
     }
   });
 }
